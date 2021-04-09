e5aff2689c91aee32778b43fdb3be75e398493c6
anzeozimek <57182861+anzeozimek@users.noreply.github.com>
2021-04-09 15:09:23 +0200

    line chart function added

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ numEvents = len(eventIds)
 start = time.time()
 events = []
 for i in range(numEvents):
-	print(f"\rDownloading eventId {eventIds[i]} ({i+1}/{numEvents})",end = "")
-	events.append(t.ReadEvent(eventIds[i]))
+    print(f"\rDownloading eventId {eventIds[i]} ({i+1}/{numEvents})",end = "")
+    events.append(t.ReadEvent(eventIds[i]))
 
 print(f"\n{(time.time() - start):.3f} s to load")
 
@@ -23,37 +23,64 @@ athletes,fisCodeMap = op.athleteResults(events)
 
 rk = athletes[fisCodeMap[2918]]
 
-ml.PredictNextJump(rk)
+pp = athletes[fisCodeMap[5658]]
 
-# x = list(range(2000,2021 + 1))
+# ml.PredictNextJump(rk)
+    
+#x = list(range(2000,2021 + 1))
 # y = [[0,0,0,0,0] for _ in range(len(x))]
-# for i in range(len(x)):
-# 	year = x[i]
-# 	for result in rk.personalResults:
-# 		if len(result) == 3 and rk.events[result[0]].competitions[result[1]].date.year == year:
-# 			rank = result[2] + 1
 
-# 			if 1 <= rank <= 3:
-# 				y[i][rank - 1] += 1
-# 			elif rank <= 10:
-# 				y[i][3] += 1
-# 			else:
-# 				y[i][4] += 1
+#for i in range(len(x)):
+#    year = x[i]
+#    for result in rk.personalResults:
+#        if len(result) == 3 and rk.events[result[0]].competitions[result[1]].date.year == year:
+#           rank = result[2] + 1
+            
+#           if 1 <= rank <= 3:
+#               y[i][rank - 1] += 1
+#           elif rank <= 10:
+#               y[i][3] += 1
+#           else:
+#               y[i][4] += 1
 
 # charts.BarChart(x,tuple(y),"Medals of Robert Kranjec","Year","Number of medals")
-
 # for result in rk.personalResults:
-# 	if len(result) == 3 and rk.events[result[0]].competitions[result[1]].date.year == 2005 and result[2] == 0:
-# 		print(rk.events[result[0]].competitions[result[1]].raceId)
-			
+#   if len(result) == 3 and rk.events[result[0]].competitions[result[1]].date.year == 2005 and result[2] == 0:
+#       print(rk.events[result[0]].competitions[result[1]].raceId)
+            
 # izbran = athletes[fisCodeMap[6392]]
 # tabTotalPoints = []
 # for res in izbran.personalResults:
-# 	if len(res) == 3:
-# 		result = izbran.events[res[0]].competitions[res[1]].results[res[2]]
-# 		if result != None:
-# 			tabTotalPoints.append(result.totalPoints)
+#   if len(res) == 3:
+#       result = izbran.events[res[0]].competitions[res[1]].results[res[2]]
+#       if result != None:
+#           tabTotalPoints.append(result.totalPoints)
 # print(tabTotalPoints)
 #print(athletes[fisCodeMap[2918]])
+#x = list(range(2000,2021 + 1))
+#tabAverages = []
+#y = []
+#tabAverages2 = []
+#y1 = []
+#for i in range(len(x)):
+#    year = x[i]
+#    for result in rk.personalResults:
+#        if (len(result) == 3 and rk.events[result[0]].competitions[result[1]].hillSizeName == 'LH' and
+#        rk.events[result[0]].competitions[result[1]].results[result[2]].totalPoints != None
+#        and rk.events[result[0]].competitions[result[1]].date.year == year):
+#            y.append(rk.events[result[0]].competitions[result[1]].results[result[2]].totalPoints)
+#    tabAverages.append(sum(y)/len(y) if len(y) != 0 else 0)
+#    y = []
+#    for result in pp.personalResults:
+#        if (len(result) == 3 and pp.events[result[0]].competitions[result[1]].hillSizeName == 'LH' and
+#        pp.events[result[0]].competitions[result[1]].results[result[2]].totalPoints != None
+#        and pp.events[result[0]].competitions[result[1]].date.year == year):
+#            y1.append(pp.events[result[0]].competitions[result[1]].results[result[2]].totalPoints)
+#    tabAverages2.append(sum(y1)/len(y1) if len(y1) != 0 else 0)
+#    y1 = []
 
+#koncnatab = []
+#koncnatab.append(tabAverages)
+#koncnatab.append(tabAverages2)
+#charts.LineChart(x,tuple(koncnatab),"Comparison of Robert Kranjec and Peter Prevc of total points in Flying hill","Year","Total points",['Robert','Peter'])
 
